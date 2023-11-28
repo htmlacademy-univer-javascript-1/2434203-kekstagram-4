0280f6a5ccc84b084f43bdd00e9ea741d2cc4899
Шандер Саша < email>
2023-11-29 00:19:05 +0500

    Рабочее время

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,13 @@
 const polyndromCheck = (string) => (string.toLowerCase().replaceAll(' ', '') === string.toLowerCase().replaceAll(' ', '').reverse())
 
 const lengthCheck = (string, length) => {return string.length >= length;};
+
+function checkWorkingTime(dayBegin, dayEnd, meetingBegin, duration) {
+  const dayBeginTime = dayBegin.split(':');
+  const dayEndTime = dayEnd.split(':');
+  const meetingBeginTime = meetingBegin.split(':');
+  const dayBeginMinutes = (+dayBeginTime[0]) * 60 + (+dayBeginTime[1]);
+  const dayEndMinutes = (+dayEndTime[0]) * 60 + (+dayEndTime[1]);
+  const meetingBeginMinutes = (+meetingBeginTime[0]) * 60 + (+meetingBeginTime[1]);
+  return dayBeginMinutes <= meetingBeginMinutes && meetingBeginMinutes + duration <= dayEndMinutes;
+};
