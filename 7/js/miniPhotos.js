f051493fa9d7eba82d2f2bdddee42dee5a921dc8
Keks <keks@htmlacademy.ru>
2023-11-29 18:57:50 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/miniPhotos.js
+++ 7/js/miniPhotos.js
@@ -1,22 +1,21 @@
 import { getPhotosArray } from './utils.js';
+import { photosCount } from './data.js';
 
 const createMiniPhotos = () => {
   const picTemp = document.querySelector('#picture').content.querySelector('.picture');
   const picContainer = document.querySelector('.pictures');
-  const photosCollection = getPhotosArray(25);
+  const photosCollection = getPhotosArray(photosCount);
   const picFragment = document.createDocumentFragment();
 
   photosCollection.forEach(({url, description, likes, comments}) => {
     const picture = picTemp.cloneNode(true);
-
     picture.querySelector('img').src = url;
     picture.querySelector('img').alt = description;
     picture.querySelector('.picture__likes').textContent = likes;
     picture.querySelector('.picture__comments').textContent = comments.length;
-
     picFragment.append(picture);
 
   });
   picContainer.append(picFragment);
-}
+};
 export {createMiniPhotos};
