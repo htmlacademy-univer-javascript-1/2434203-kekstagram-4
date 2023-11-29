cf9c89de8c184a3d1c62b5d458ac6d30bcc7a623
Шандер Саша < email>
2023-11-29 18:41:04 +0500

    Отрисуй меня полностью

---
+++ js/miniPhotos.js
@@ -0,0 +1,21 @@
+import { getPhotosArray } from './utils.js';
+
+const createMiniPhotos = () => {
+  const picTemp = document.querySelector('#picture').content.querySelector('.picture');
+  const picContainer = document.querySelector('.pictures');
+  const photosCollection = getPhotosArray(25);
+  const picFragment = document.createDocumentFragment();
+
+  photosCollection.forEach(({url, description, likes, comments}) => {
+    const picture = picTemp.cloneNode(true);
+
+    picture.querySelector('img').src = url;
+    picture.querySelector('img').alt = description;
+    picture.querySelector('.picture__likes').textContent = likes;
+    picture.querySelector('.picture__comments').textContent = comments.length;
+
+    picFragment.append(picture);
+  });
+  picContainer.append(picFragment);
+}
+export {createMiniPhotos};
