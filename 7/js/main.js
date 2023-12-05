f4fae516abbe057291d6a483839089bd2aa76760
Keks <keks@htmlacademy.ru>
2023-12-05 15:13:25 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/main.js
+++ 7/js/main.js
@@ -1,7 +1,5 @@
-import { createMiniPhotos } from "./mini-photos.js";
+import { renderMiniPhotos } from './mini-photos.js';
 import { getPhotosArray } from './utils.js';
-import { photosCount } from './data.js';
 
-const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const photosCollection = getPhotosArray(photosCount);
-createMiniPhotos(pictureTemplate, photosCollection)
+const photosCollection = getPhotosArray();
+renderMiniPhotos(photosCollection);
