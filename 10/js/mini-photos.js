f0e31ce7f11aec35645d5de0fba180812ff17df4
Keks <keks@htmlacademy.ru>
2023-12-20 20:10:38 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/mini-photos.js
@@ -0,0 +1,23 @@
+import { openViewPopup } from './view-popup.js';
+
+const picContainer = document.querySelector('.pictures');
+
+const renderMiniPhotos = (photosCollection) => {
+  const picFragment = document.createDocumentFragment();
+
+  photosCollection.forEach(({url, description, likes, comments}) => {
+    const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+    const picture = pictureTemplate.cloneNode(true);
+    picture.querySelector('img').src = url;
+    picture.querySelector('img').alt = description;
+    picture.querySelector('.picture__likes').textContent = likes;
+    picture.querySelector('.picture__comments').textContent = comments.length;
+    picture.addEventListener('click', () => {
+      openViewPopup(url, description, likes, comments);
+    });
+    picFragment.append(picture);
+  });
+  picContainer.append(picFragment);
+}
+
+export {renderMiniPhotos};
